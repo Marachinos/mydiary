c5cf672adac6bd10638d895653cdb61c3d010d2b
Marachinos <stallnysater@gmail.com>
2025-09-25 19:12:18 +0200

    //6 diffrent cases and a question if you want                     //to save changes before closing the app

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Xml.Linq;
 
 namespace mydiary
 {
@@ -22,6 +23,45 @@ namespace mydiary
             Console.WriteLine("Välkommen till min lilla enkla dagbok :)\n");
 
             LoadFromFile();
-        }
+
+            while (true)
+            {
+                ShowMenu();
+                string choice = Console.ReadLine();
+                switch (choice)
+                {   //6 diffrent cases and a question if you want
+                    //to save changes before closing the app
+                    case "1":
+                        AddEntry();
+                        break;
+                    case "2":
+                        ListEntries();
+                        break;
+                    case "3":
+                        SearchByDate();
+                        break;
+                    case "4":
+                        SaveToFile();
+                        break;
+                    case "5":
+                        LoadFromFile();
+                        break;
+                    case "6":
+                        Console.WriteLine("Vill du spara ändringarna innan du avslutar? (j/n)");
+                        if (Console.ReadLine()?.ToLower() == j)
+                        {
+                            SaveToFile();
+                        }
+                        Console.WriteLine("Avslutar programmet.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Ogiltigt val, ange 1-6 och försök igen.");
+                        break;
+                }
+                Console.WriteLine();
+                }
+            }
+
     }
 }
