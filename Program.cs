6070d5e4d3a1efc6b3744970401f0107160f71ed
Marachinos <stallnysater@gmail.com>
2025-09-25 18:25:19 +0200

    Gitignore fixad. Diary Entry Class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-﻿namespace mydiary
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace mydiary
 {
-    internal class Program
+    public class diaryEntry //Diary Entry Class
+    { 
+    public DateTime date { get; set; }
+        public string name { get; set; }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
